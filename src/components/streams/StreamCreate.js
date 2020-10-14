2dcfa17fc2a6c98bc754b967e7f50915b28f0d14
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2020-10-14 19:10:55 +0300

    Add StreamCreate input
    - wire up Redux Form
    - handle form input
    - validate input feature

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,85 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+
+  renderError ({ error, touched }) {
+    // touched is eventListener from redux-form for clicking out of Field
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="field">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`
+    // take ALL the formProps.input props provided by Field and assign them as input properties
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="Name of your stream:"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Describe your stream:"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "Title is missing!";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Provide a description!";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  // give form a meaningful name
+  form: "streamCreate",
+  // wire up validate
+  validate,
+})(StreamCreate);
+
+// reduxForm is somewhat similar to connect function from "react-redux"
+// Field is a wrapper React component that will handle form inputs
+// Field passes formProps (key-value pairs) to renderInput and we leverage them to assign to  input properties:
+//  <input onChange={formProps.input.onChange} value={} .../>
+// Field passes all to renderInput all unfamiliar properties as well (label for example)
+// Redux-form takes care of the rest. All inputs are in store now!
+// Handled inputs are nested in store, form -> streamCreate -> values
+// handleSubmit prevents default on form and passes to onSubmit helper formValues
+// redux-form runs validate every time user input changes, it is expected to return errors object
+// errors object entries are available on meta.error for according Field names together with meta.touched and much more
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
 
+// formReducer wires up redux-form to app
