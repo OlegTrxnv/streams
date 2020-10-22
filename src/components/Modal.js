668453e3280653837ad9b338a4b703ab669aa3a4
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2020-10-22 14:28:25 +0300

    Make modal reusable
    - refactor modal to be reusable
    - wire up to StreamDelete

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,25 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
     <div
-      // hide modal
-      onClick={() => history.push("/")}
+      // hide modal on click
+      onClick={props.onDismiss}
       className="ui dimmer modals visible active"
     >
       <div
-        // do not hide modal on click
+        // do not hide modal when click on modal message
         onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete stream</div>
-        <div className="content">Are you sure you want to delete stream?</div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
